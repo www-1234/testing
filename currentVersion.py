810fa9668afe88042ce93fd39eff55b5733cb036
www-1234 <137902715+www-1234@users.noreply.github.com>
2023-07-03 00:46:18 -0400

    Update currentVersion.py

--- currentVersion.py
+++ currentVersion.py
@@ -1,3 +1,5 @@
+import random
+
 print("Try-N-Go\n---------------------------------------")
 gameBoard = [
   [0],
@@ -22,45 +24,69 @@ def printBoard(x,y):
 turnCounter = 0
 p = 0 
 def inputMove(p):
-  while True:
-    r = int(input("Input Row: "))-1
-    c = int(input("Input Column: "))-1
-    try:
-      if r < 0:
-        raise IndexError
-      elif c < 0:
-        raise IndexError
-      if gameBoard[r][c] == 0:
-        gameBoard[r][c] = (p+1)
+  if p == 0:
+    while True:
+      r = int(input("Input Row: "))-1
+      c = int(input("Input Column: "))-1
+      try:
+        if r < 0:
+          raise IndexError
+        elif c < 0:
+          raise IndexError
+        if gameBoard[r][c] == 0:
+          gameBoard[r][c] = (p+1)
+        else:
+          raise ValueError
+      except ValueError:
+        print("Error: Spot Already Taken")
+      except IndexError:
+        print("Error: Outside of Range")
       else:
-        raise ValueError
-    except ValueError:
-      print("Error: Spot Already Taken")
-    except IndexError:
-      print("Error: Outside of Range")
-    else:
-      break
+        break
+  if p == 1:
+    while True:
+      r = random.randint(0,5)
+      c = random.randint(0,(r*2))
+      try:
+        if r < 0:
+          raise IndexError
+        elif c < 0:
+          raise IndexError
+        if gameBoard[r][c] == 0:
+          gameBoard[r][c] = (p+1)
+          print(f"CPU Input: [{r+1}, {c+1}]")
+        else:
+          raise ValueError
+      except ValueError:
+        pass
+      except IndexError:
+        pass
+      else:
+        break
   printBoard(x,y)
 
 assignTo = 0
 pointsA = 0
 pointsB = 0
 
-def claimTriangle(p):
-  while True:
-      x = str(input("Claim Triangle ('y' or 'n'): "))
-      if x == "y" or x == "n":
-        break
+def claimTriangle(pointVal, p):
+  if assignTo == 1 and p == 0:
+    while True:
+        x = str(input("Claim Triangle ('y' or 'n'): "))
+        if x == "y" or x == "n":
+          break
+  if assignTo == 2 and p == 1:
+    x = "y"
   if x == "y":
     if assignTo == 1:
       global pointsA
-      pointsA += p
+      pointsA += pointVal
     elif assignTo == 2:
       global pointsB
-      pointsB += p
+      pointsB += pointVal
     return 1
 
-def checkForT1A():
+def checkForT1A(p):
   a = 0
   b = 0
   try:
@@ -71,15 +97,15 @@ def checkForT1A():
             print(f"Triangle [{a+1},{b+1}] [{a+2},{b+1}] [{a+2},{b+2}] [{a+2},{b+3}] Filled")
             list = [gameBoard[a][b], gameBoard[a+1][b], gameBoard[a+1][b+1], gameBoard[a+1][b+2]]
             if list.count(1) > list.count(2):
-                print("Majority 1")
+                print("Player 1:")
                 global assignTo
                 assignTo = 1
-                if claimTriangle(2) == 1:
+                if claimTriangle(2, p) == 1:
                   gameBoard[a][b] = gameBoard[a+1][b] = gameBoard[a+1][b+1] = gameBoard[a+1][b+2] = 0
             elif list.count(2) > list.count(1):
-                print("Majority 2")
+                print("Player 2:")
                 assignTo = 2
-                if claimTriangle(2) == 1:
+                if claimTriangle(2, p) == 1:
                   gameBoard[a][b] = gameBoard[a+1][b] = gameBoard[a+1][b+1] = gameBoard[a+1][b+2] = 0
         
         b += 2
@@ -87,7 +113,7 @@ def checkForT1A():
   except:
     pass
 
-def checkForT1B():
+def checkForT1B(p):
     a = 3
     b = 3
     try:
@@ -98,22 +124,22 @@ def checkForT1B():
               print(f"Triangle [{a},{b-1}] [{a},{b}] [{a},{b+1}] [{a+1},{b+1}] Filled")
               list = [gameBoard[a][b], gameBoard[a-1][b], gameBoard[a-1][b-1], gameBoard[a-1][b-2]]
               if list.count(1) > list.count(2):
-                  print("Majority 1")
+                  print("Player 1:")
                   global assignTo
                   assignTo = 1
-                  if claimTriangle(2) == 1:
+                  if claimTriangle(2, p) == 1:
                     gameBoard[a][b] = gameBoard[a-1][b] = gameBoard[a-1][b-1] = gameBoard[a-1][b-2] = 0
               elif list.count(2) > list.count(1):
-                  print("Majority 2")
+                  print("Player 2:")
                   assignTo = 2
-                  if claimTriangle(2) == 1:
+                  if claimTriangle(2, p) == 1:
                     gameBoard[a][b] = gameBoard[a-1][b] = gameBoard[a-1][b-1] = gameBoard[a-1][b-2] = 0
           b += 2
         a += 1
     except:
       pass
 
-def checkForT2A():
+def checkForT2A(p):
   a = 0
   b = 0
   while a < (len(gameBoard)-2):
@@ -123,34 +149,34 @@ def checkForT2A():
         print(f"Triangle [{a+1},{b+1}] [{a+2},{b+1}] [{a+2},{b+2}] [{a+2},{b+3}] [{a+3},{b+1}] [{a+3},{b+2}] [{a+3},{b+3}] [{a+3},{b+4}] [{a+3},{b+5}] Filled")
         list = [gameBoard[a][b], gameBoard[a+1][b], gameBoard[a+1][b+1], gameBoard[a+1][b+2], gameBoard[a+2][b], gameBoard[a+2][b+1], gameBoard[a+2][b+2], gameBoard[a+2][b+3], gameBoard[a+2][b+4]]
         if list.count(1) > list.count(2):
-          print("Majority 1")
+          print("Player 1:")
           global assignTo
           assignTo = 1
-          if claimTriangle(5) == 1:
+          if claimTriangle(5, p) == 1:
             gameBoard[a][b] = gameBoard[a+1][b] = gameBoard[a+1][b+1] = gameBoard[a+1][b+2] = gameBoard[a+2][b] = gameBoard[a+2][b+1] = gameBoard[a+2][b+2] = gameBoard[a+2][b+3] = gameBoard[a+2][b+4] = 0
         elif list.count(2) > list.count(1):
-          print("Majority 2")
+          print("Player 2:")
           assignTo = 2
-          if claimTriangle(5) == 1:
+          if claimTriangle(5, p) == 1:
             gameBoard[a][b] = gameBoard[a+1][b] = gameBoard[a+1][b+1] = gameBoard[a+1][b+2] = gameBoard[a+2][b] = gameBoard[a+2][b+1] = gameBoard[a+2][b+2] = gameBoard[a+2][b+3] = gameBoard[a+2][b+4] = 0
       b += 2
     a += 1
 
 
-def checkForT2B():
+def checkForT2B(p):
   if (gameBoard[3][5] and gameBoard[3][4] and gameBoard[3][3] and gameBoard[3][2] and gameBoard[3][1] and gameBoard[4][4] and gameBoard[4][5] and gameBoard[4][3] and gameBoard[5][5]) != 0:
     print("Triangle [4,2] [4,3] [4,4] [4,5] [4,6] [5,4] [5,5] [5,6] [6,6] Filled")
     list = [gameBoard[3][5], gameBoard[3][4], gameBoard[3][3], gameBoard[3][2], gameBoard[3][1], gameBoard[4][4], gameBoard[4][5], gameBoard[4][3], gameBoard[5][5]]
     if list.count(1) > list.count(2):
-      print("Majority 1")
+      print("Player 1:")
       global assignTo
       assignTo = 1
-      if claimTriangle(5) == 1:
+      if claimTriangle(5, p) == 1:
         gameBoard[3][5] = gameBoard[3][4] = gameBoard[3][3] = gameBoard[3][2] = gameBoard[3][1] = gameBoard[4][4] = gameBoard[4][5] = gameBoard[4][3] = gameBoard[5][5] = 0
     elif list.count(2) > list.count(1):
-      print("Majority 2")
+      print("Player 2:")
       assignTo = 2
-      if claimTriangle(5) == 1:
+      if claimTriangle(5, p) == 1:
         gameBoard[3][5] = gameBoard[3][4] = gameBoard[3][3] = gameBoard[3][2] = gameBoard[3][1] = gameBoard[4][4] = gameBoard[4][5] = gameBoard[4][3] = gameBoard[5][5] = 0
 
 def playGame(p, turnCounter):
@@ -160,15 +186,16 @@ def playGame(p, turnCounter):
     print(f"Turn {turnCounter+1}")
     print(f"Player {p+1}'s Move")
     inputMove(p)
-    checkForT1A()
-    checkForT1B()
-    checkForT2A()
-    checkForT2B()
+    checkForT1A(p)
+    checkForT1B(p)
+    checkForT2A(p)
+    checkForT2B(p)
     print(f"P1 Points: {pointsA}\nP2 Points: {pointsB}")
     turnCounter+=1
     print("\n---------------------------------------")
-    if turnCounter == 45:
+    if turnCounter == 46:
       break
+
 playGame(p, turnCounter)
 if pointsA > pointsB:
   print("Game Over: Player 1 Wins")
